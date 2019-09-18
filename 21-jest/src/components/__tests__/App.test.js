60406ceb4a38806811ec46bc9b65bf7702c82724
Andrii <radomely@gmail.com>
2019-09-18 20:38:13 +0300

    test

--- 21-jest/src/components/__tests__/App.test.js
+++ 21-jest/src/components/__tests__/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
+import TodoEditor from "../TodoEditor";
+import TodoList from "../TodoList";
 
 it("App renders a TodoEditor", () => {
   const wrapped = shallow(<App />);
+
+  expect(wrapped.find(TodoEditor).length).toEqual(1);
+});
+
+it("App does not render a TodoList when there are no todos", () => {
+  const wrapped = shallow(<App />);
+
+  wrapped.setState({ todos: [] });
+
+  expect(wrapped.find(TodoEditor).length).toBe(0);
 });
