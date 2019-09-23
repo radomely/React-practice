0de8bb9612a38b71a7c976c0f6aa3ddd4a69a426
Andrii <radomely@gmail.com>
2019-09-23 17:46:08 +0300

    test

--- 21-jest/src/components/App.js
+++ 21-jest/src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import TodoEditor from "./TodoEditor";
-import TodoList from "./TodoList";
+import React, { Component } from 'react';
+import TodoEditor from './TodoEditor';
+import TodoList from './TodoList';
 
 export default class App extends Component {
   state = {
@@ -16,11 +16,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <span>wreerw</span>
         <TodoEditor onSave={this.saveTodo} />
-        <TodoList items={this.state.todos} />
-        <span>qweqwe</span>
-        {/* {this.state.todos.length > 0 && <TodoList items={this.state.todos} />} */}
+
+        {this.state.todos.length > 0 && <TodoList items={this.state.todos} />}
       </div>
     );
   }
