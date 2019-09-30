7f290b810396e4d4cf7f2d03e3f4011415ac8e54
Andrii <radomely@gmail.com>
2019-09-30 18:03:31 +0300

    test

--- 21-jest/src/components/__tests__/App.test.js
+++ 21-jest/src/components/__tests__/App.test.js
@@ -10,7 +10,20 @@ describe("App component", () => {
   beforeEach(() => {
     wrapped = shallow(<App />);
   });
+
   it("renders a TodoEditor", () => {
     expect(wrapped.find(TodoEditor).length).toBe(1);
+  });
+
+  it("does not render a TodoList when there are no todos", () => {
+    wrapped.setState({ todos: [] });
+
+    expect(wrapped.find(TodoList).length).toBe(0);
+  });
+
+  it("renders a TodoList when there is at least one todo", () => {
+    wrapped.setState({ todos: ["first todo"] });
+
+    expect(wrapped.find(TodoList).length).toBe(1);
   });
 });
