a3af297a397ec51b0eea42edbdb19bd40f07cee7
Andrii <radomely@gmail.com>
2019-10-01 18:00:02 +0300

    test

--- 21-jest/src/components/__tests__/TodoEditor.test.js
+++ 21-jest/src/components/__tests__/TodoEditor.test.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import TodoEditor from '../TodoEditor';
+import React from "react";
+import { shallow } from "enzyme";
+import TodoEditor from "../TodoEditor";
 
-describe('TodoEditor', () => {
+describe("TodoEditor", () => {
   let wrapped;
-
   beforeEach(() => {
     wrapped = shallow(<TodoEditor />);
   });
 
-  it('renders an input and a button', () => {
+  it("renders an input and a button", () => {
     expect(wrapped.find('input[type="text"]').length).toBe(1);
     expect(wrapped.find('button[type="submit"]').length).toBe(1);
   });
 
-  describe('events', () => {
-    const todoText = 'new todo text';
+  describe("events", () => {
+    const todoText = "new todo text";
 
     beforeEach(() => {
       const changeEventMock = {
@@ -24,16 +23,16 @@ describe('TodoEditor', () => {
         }
       };
 
-      wrapped.find('input[type="text"]').simulate('change', changeEventMock);
+      wrapped.find('input[type="text"]').simulate("change", changeEventMock);
       wrapped.update();
     });
 
-    it('can enter text into an input', () => {
-      expect(wrapped.find('input[type="text"]').prop('value')).toBe(todoText);
+    it("can enter text into an input", () => {
+      expect(wrapped.find('input[type="text"]').prop("value")).toBe(todoText);
     });
 
-    it('on form submit, input should get emptied', () => {
-      expect(wrapped.find('input[type="text"]').prop('value')).toBe(todoText);
+    it("on form submit, input should get emptied", () => {
+      expect(wrapped.find('input[type="text"]').prop("value")).toBe(todoText);
 
       const submitEventMock = {
         preventDefault: () => null
@@ -43,14 +42,14 @@ describe('TodoEditor', () => {
       };
 
       wrapped.setProps(props);
-      wrapped.find('form').simulate('submit', submitEventMock);
+      wrapped.find("form").simulate("submit", submitEventMock);
       wrapped.update();
-      expect(wrapped.find('input[type="text"]').prop('value')).toBe('');
+      expect(wrapped.find('input[type="text"]').prop("value")).toBe("");
     });
   });
 
-  describe('callbacks', () => {
-    it('onSave prop has been called on submit', () => {
+  describe("callbacks", () => {
+    it("onSave prop has been called on submit", () => {
       const submitEventMock = {
         preventDefault: () => null
       };
@@ -61,7 +60,7 @@ describe('TodoEditor', () => {
       };
 
       wrapped.setProps(props);
-      wrapped.find('form').simulate('submit', submitEventMock);
+      wrapped.find("form").simulate("submit", submitEventMock);
 
       expect(onSavePropMock.mock.calls.length).toBe(1);
     });
